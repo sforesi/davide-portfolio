5ae67dac2f1e1cbe9fb58897059ee417fd0d2445
sforesi <sebastiano.foresi@gmail.com>
2022-02-08 14:46:00 -0500

    services and press stub up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Press from './pages/Press';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-
+        <Route path='/press' element={<Press />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Press.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Press = () => {
+  return (
+    <div>
+      HES FAMOUS 
+    </div>
+  );
+};
+
+export default Press;
