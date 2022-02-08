85366a7ebe95c459192472a1d4270674b348cf46
sforesi <sebastiano.foresi@gmail.com>
2022-02-08 13:58:48 -0500

    stub up nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      About 
+      Services 
+      Press
+      <button>Make a reservation</button>
+    </div>
+  );
+};
+
+export default Nav;
