649306dbd3349a7450e92d390a8aae11dbbaa469
sforesi <sebastiano.foresi@gmail.com>
2022-02-26 11:21:32 -0500

    add services

--- src/pages/services./Services.js
+++ src/pages/services./Services.js
@@ -3,10 +3,16 @@ import React from 'react';
 const Services = () => {
   return (
     <div>
-      Sound Recording 
-      Mixing 
-      Music lessos 
-      Live Performances
+      <h1>Sound Recording:</h1>
+        <p>Aute qui duis voluptate exercitation sint nostrud duis cillum labore enim minim adipisicing Lorem. Eiusmod ut ex dolor enim voluptate elit velit commodo in irure. Officia et eiusmod Lorem laborum labore id ea sint dolore commodo. Eiusmod id anim excepteur do ad exercitation eu aute ullamco ad tempor. Labore ea Lorem consectetur nisi. Sint sint aliquip laborum nostrud.</p>
+      <h1>Mixing:</h1>
+        <p>Aute qui duis voluptate exercitation sint nostrud duis cillum labore enim minim adipisicing Lorem. Eiusmod ut ex dolor enim voluptate elit velit commodo in irure. Officia et eiusmod Lorem laborum labore id ea sint dolore commodo. Eiusmod id anim excepteur do ad exercitation eu aute ullamco ad tempor. Labore ea Lorem consectetur nisi. Sint sint aliquip laborum nostrud.</p>
+      <h1>Live Performances:</h1>
+        <p>Aute qui duis voluptate exercitation sint nostrud duis cillum labore enim minim adipisicing Lorem. Eiusmod ut ex dolor enim voluptate elit velit commodo in irure. Officia et eiusmod Lorem laborum labore id ea sint dolore commodo. Eiusmod id anim excepteur do ad exercitation eu aute ullamco ad tempor. Labore ea Lorem consectetur nisi. Sint sint aliquip laborum nostrud.</p>
+        <h1>Music Instructor:</h1>
+        <p>Aute qui duis voluptate exercitation sint nostrud duis cillum labore enim minim adipisicing Lorem. Eiusmod ut ex dolor enim voluptate elit velit commodo in irure. Officia et eiusmod Lorem laborum labore id ea sint dolore commodo. Eiusmod id anim excepteur do ad exercitation eu aute ullamco ad tempor. Labore ea Lorem consectetur nisi. Sint sint aliquip laborum nostrud.</p>
+
+      
     </div>
   );
 };
